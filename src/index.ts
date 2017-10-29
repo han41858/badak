62fd145ff94f62cb7371ed3019b7c7643dff1a27
Janghyun Han <han41858@gmail.com>
2017-10-30 08:32:04 +0900

    use() function added

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
+// import * as http from 'http';
+// import { Server } from 'net';
+
 export class Badak {
-	async listen (port : number) : Promise<any> {
+	private _middleware : Promise<Function>[] = [];
+
+	async use (middleware : Promise<Function>) : Promise<void> {
+		if (middleware === undefined) {
+			throw new Error('middleware function should be passed');
+		}
+
+		if (middleware.constructor.name !== 'AsyncFunction') {
+			throw new Error('middleware function should be async');
+		}
+
+		this._middleware.push(middleware);
+	}
+
+	async listen (port : number) : Promise<void> {
 		if (port === undefined) {
 			throw new Error('port should be passed');
 		}
@@ -7,7 +24,5 @@ export class Badak {
 		if (typeof port !== 'number') {
 			throw new Error('port should be number type');
 		}
-
-		return {};
 	}
 }
