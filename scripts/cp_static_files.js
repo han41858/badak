a753093a8c82d0656bded1fe84a69d463438f4ff
Janghyun Han <han41858@gmail.com>
2020-03-02 18:53:58 +0900

    copying test file logic refactoring

---
+++ scripts/cp_static_files.js
@@ -0,0 +1,27 @@
+// cp -rf spec/static build/spec/static
+const { existsSync, mkdirSync, copyFileSync, readdirSync, lstatSync } = require('fs');
+
+const cp = (src, dest) => {
+	if (existsSync(src)) {
+		if (!existsSync(dest)) {
+			mkdirSync(dest);
+		}
+		
+		const files = readdirSync(src);
+		
+		files.forEach(fileOrDir => {
+			const stats = lstatSync(`${src}/${fileOrDir}`);
+			
+			if (stats.isDirectory()) {
+				// call recursively
+				cp(`${src}/${fileOrDir}`, `${dest}/${fileOrDir}`);
+			} else {
+				copyFileSync(`${src}/${fileOrDir}`, `${dest}/${fileOrDir}`);
+			}
+		});
+	} else {
+		throw new Error(`no src path: ${src}`);
+	}
+};
+
+cp('./spec/static', './build/spec/static');
