e5f4d9ace74d40445db265fe397f593c1ff8513e
han41858 <han41858@gmail.com>
2021-07-15 19:43:54 +0900

    RouteRuleSeed added into public_api.ts

--- src/public_api.ts
+++ src/public_api.ts
@@ -1,3 +1,3 @@
 export { Badak } from './badak';
 export { Method } from './constants';
-export { BadakOption, RouteRule, RouteFunction, MiddlewareFunction, RouteOption } from './interfaces';
+export { BadakOption, RouteRule, RouteFunction, RouteRuleSeed, MiddlewareFunction, RouteOption } from './interfaces';
